e7fae27752a30bc210c507ec20df715fb5d1d004
rkeplin <rkeplin@gmail.com>
2013-08-28 22:17:29 +0000

    added view stuff to global file

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -25,4 +25,9 @@ return array(
             ),
         ),
     ),
+
+    'view_manager' => array(
+        'display_not_found_reason' => false,
+        'display_exceptions'       => false,
+    )
 );
